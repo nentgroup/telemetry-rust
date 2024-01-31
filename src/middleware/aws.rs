8469f481da6dc28272d1a8ad6c3fa0ca6b80c796
Jayanthi Padmanabhan <jayanthi.padmanabhan@viaplaygroup.com>
2024-01-31 20:09:04 +0100

    add rpc attributes

--- src/middleware/aws.rs
+++ src/middleware/aws.rs
@@ -2,9 +2,8 @@ use opentelemetry::{
     global,
     trace::{SpanKind, Tracer},
 };
-use tracing_opentelemetry_instrumentation_sdk::TRACING_TARGET;
-
 pub use opentelemetry::{trace::Span, KeyValue};
+pub use opentelemetry_semantic_conventions as semcov;
 
 // TODO: Write as macro
 //
@@ -28,16 +27,22 @@ pub fn info_span_dynamo(
     let tracer = global::tracer("aws_sdk");
     let mut span = tracer
         .span_builder("aws_dynamo")
+        .with_attributes(vec![
+            semcov::trace::RPC_METHOD.string(method.to_string()),
+            semcov::trace::RPC_SYSTEM.string("aws-api"),
+            semcov::trace::RPC_SERVICE.string("DynamoDB"),
+        ])
         .with_kind(SpanKind::Client)
         .start_with_context(&tracer, parent_context);
-    span.set_attribute(KeyValue::new("aws_dynamoDB", true));
+
+    span.set_attribute(KeyValue::new("dynamoDB", true));
     span.set_attribute(KeyValue::new("aws_operation", operation.to_string()));
-    span.set_attribute(KeyValue::new("aws_tableName", table_name.to_string()));
-    span.set_attribute(KeyValue::new("aws_method", method.to_string()));
-    span.set_attribute(KeyValue::new("aws_service", "AWS::DynamoDB"));
+    span.set_attribute(KeyValue::new("db.name", table_name.to_string()));
+    span.set_attribute(KeyValue::new("db.system", "dynamodb"));
+    span.set_attribute(KeyValue::new("db.operation", method.to_string()));
 
     if let Some(r) = config.region() {
-        span.set_attribute(KeyValue::new("Region_val", r.to_string()));
+        span.set_attribute(KeyValue::new("cloud.region", r.to_string()));
     }
     span
 }
@@ -48,32 +53,35 @@ pub fn info_span_firehose(
     firehose_stream_name: &str,
     operation: &str,
     method: &str,
-) -> tracing::Span {
+    parent_context: &opentelemetry::Context,
+) -> opentelemetry::global::BoxedSpan {
     // Spans will be sent to the configured OpenTelemetry exporter
     // use telemetry_rust::OpenTelemetrySpanExt;
     let config = firehose_client.config();
-    if let Some(region) = config.region() {
-        let span = tracing::info_span!(
-            target: TRACING_TARGET,
-            "aws_firehose",
-            firehose = tracing::field::Empty,
-            operation = tracing::field::Empty,
-            firehose_stream_name = tracing::field::Empty,
-            method = tracing::field::Empty,
-            service = tracing::field::Empty,
-            cloud.region = tracing::field::Empty,
-            success = tracing::field::Empty,
-        );
-        span.record("firehose", "true");
-        span.record("operation", operation);
-        span.record("firehose_stream_name", firehose_stream_name);
-        span.record("method", method);
-        span.record("service", "AWS::Firehose");
-        span.record("cloud.region", region.as_ref());
-        span
-    } else {
-        tracing::Span::none()
+
+    let tracer = global::tracer("aws_sdk");
+    let mut span = tracer
+        .span_builder("aws_firehose")
+        .with_attributes(vec![
+            semcov::trace::RPC_METHOD.string(method.to_string()),
+            semcov::trace::RPC_SYSTEM.string("aws-api"),
+            semcov::trace::RPC_SERVICE.string("Firehose"),
+        ])
+        .with_kind(SpanKind::Client)
+        .start_with_context(&tracer, parent_context);
+
+    span.set_attribute(KeyValue::new("firehose", true));
+    span.set_attribute(KeyValue::new(
+        "firehose.name",
+        firehose_stream_name.to_string(),
+    ));
+    span.set_attribute(KeyValue::new("system", "firehose"));
+    span.set_attribute(KeyValue::new("operation", operation.to_string()));
+
+    if let Some(r) = config.region() {
+        span.set_attribute(KeyValue::new("cloud.region", r.to_string()));
     }
+    span
 }
 
 #[cfg(any(feature = "aws", feature = "aws_sns"))]
@@ -81,28 +89,29 @@ pub fn info_span_sns(
     sns_client: &aws_sdk_sns::Client,
     operation: &str,
     method: &str,
-) -> tracing::Span {
+    parent_context: &opentelemetry::Context,
+) -> opentelemetry::global::BoxedSpan {
     // Spans will be sent to the configured OpenTelemetry exporter
     // use telemetry_rust::OpenTelemetrySpanExt;
     let config = sns_client.config();
-    if let Some(region) = config.region() {
-        let span = tracing::info_span!(
-            target: TRACING_TARGET,
-            "aws_sns",
-            sns = tracing::field::Empty,
-            operation = tracing::field::Empty,
-            method = tracing::field::Empty,
-            service = tracing::field::Empty,
-            cloud.region = tracing::field::Empty,
-            success = tracing::field::Empty,
-        );
-        span.record("SNS", "true");
-        span.record("operation", operation);
-        span.record("method", method);
-        span.record("service", "AWS::SNS");
-        span.record("cloud.region", region.as_ref());
-        span
-    } else {
-        tracing::Span::none()
+
+    let tracer = global::tracer("aws_sdk");
+    let mut span = tracer
+        .span_builder("aws_sns")
+        .with_attributes(vec![
+            semcov::trace::RPC_METHOD.string(method.to_string()),
+            semcov::trace::RPC_SYSTEM.string("aws-api"),
+            semcov::trace::RPC_SERVICE.string("SNS"),
+        ])
+        .with_kind(SpanKind::Client)
+        .start_with_context(&tracer, parent_context);
+
+    span.set_attribute(KeyValue::new("sns", true));
+    span.set_attribute(KeyValue::new("system", "sns"));
+    span.set_attribute(KeyValue::new("operation", operation.to_string()));
+
+    if let Some(r) = config.region() {
+        span.set_attribute(KeyValue::new("cloud.region", r.to_string()));
     }
+    span
 }

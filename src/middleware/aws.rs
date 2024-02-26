d937c831c628ad64b1fa8ae3d138b3c540067dcf
Jayanthi Padmanabhan <jayanthi.padmanabhan@viaplaygroup.com>
2024-02-26 15:53:01 +0100

    add awsSpan as generic

--- src/middleware/aws.rs
+++ src/middleware/aws.rs
@@ -1,7 +1,7 @@
 use aws_types::request_id::RequestId;
 use opentelemetry::{
-    global::{self, BoxedSpan},
-    trace::{Span as TelemetrySpan, SpanKind, Status, Tracer},
+    global::{self, BoxedSpan, BoxedTracer},
+    trace::{Span as TelemetrySpan, SpanBuilder, SpanKind, Status, Tracer},
     Context, KeyValue,
 };
 use opentelemetry_semantic_conventions as semcov;
@@ -42,48 +42,70 @@ impl AwsTarget<'_> {
     }
 }
 
-pub fn create_aws_span(
-    aws_target: AwsTarget,
-    operation: &str,
-    method: &str,
-    parent_context: Option<&Context>,
-) -> opentelemetry::global::BoxedSpan {
-    let tracer = global::tracer("aws_sdk");
-    let system = aws_target.system();
-    let mut attributes = vec![
-        semcov::trace::RPC_METHOD.string(method.to_string()),
-        semcov::trace::RPC_SYSTEM.string("aws-api"),
-        semcov::trace::RPC_SERVICE.string(system),
-        KeyValue::new("aws_operation", operation.to_string()),
-        KeyValue::new("db.system", system),
-        KeyValue::new("db.operation", method.to_string()),
-    ];
-    attributes.extend(aws_target.attributes());
-    let span = tracer
-        .span_builder(format!("aws_{system}"))
-        .with_attributes(attributes)
-        .with_kind(SpanKind::Client);
-    match parent_context {
-        Some(ctx) => span.start_with_context(&tracer, ctx),
-        None => span.start_with_context(&tracer, &Span::current().context()),
-    }
+pub enum AwsSpan {
+    Pending(SpanBuilder, BoxedTracer),
+    Started(BoxedSpan),
 }
 
-pub fn end_aws_span<T, E>(mut span: BoxedSpan, aws_response: &Result<T, E>)
-where
-    T: RequestId,
-    E: RequestId + Error,
-{
-    let (status, request_id) = match aws_response {
-        Ok(resp) => (Status::Ok, resp.request_id()),
-        Err(error) => {
-            span.record_error(&error);
-            (Status::error(error.to_string()), error.request_id())
-        }
-    };
-    if let Some(value) = request_id {
-        span.set_attribute(semcov::trace::AWS_REQUEST_ID.string(value.to_owned()));
+impl AwsSpan {
+    pub fn new(aws_target: AwsTarget, operation: &str, method: &str) -> Self {
+        let tracer = global::tracer("aws_sdk");
+        let system = aws_target.system();
+        let mut attributes = vec![
+            semcov::trace::RPC_METHOD.string(method.to_string()),
+            semcov::trace::RPC_SYSTEM.string("aws-api"),
+            semcov::trace::RPC_SERVICE.string(system),
+            KeyValue::new("aws_operation", operation.to_string()),
+            KeyValue::new("db.system", system),
+            KeyValue::new("db.operation", method.to_string()),
+        ];
+        attributes.extend(aws_target.attributes());
+        let span_builder = tracer
+            .span_builder(format!("aws_{system}"))
+            .with_attributes(attributes)
+            .with_kind(SpanKind::Client);
+
+        Self::Pending(span_builder, tracer)
     }
-    span.set_attribute(KeyValue::new("success", status == Status::Ok));
-    span.set_status(status);
+
+    pub fn start_with_context(self, parent_cx: &Context) -> Self {
+        let span = match self {
+            AwsSpan::Pending(builder, tracer) => {
+                builder.start_with_context(&tracer, parent_cx)
+            }
+            AwsSpan::Started(_) => {
+                panic!("Span already started");
+            }
+        };
+        AwsSpan::Started(span)
+    }
+
+    pub fn start(self) -> Self {
+        self.start_with_context(&Span::current().context())
+    }
+
+    pub fn end<T, E>(self, aws_response: &Result<T, E>)
+    where
+        T: RequestId,
+        E: RequestId + Error,
+    {
+        let mut span = match self {
+            AwsSpan::Pending(_, _) => {
+                panic!("Span not started");
+            }
+            AwsSpan::Started(span) => span,
+        };
+        let (status, request_id) = match aws_response {
+            Ok(resp) => (Status::Ok, resp.request_id()),
+            Err(error) => {
+                span.record_error(&error);
+                (Status::error(error.to_string()), error.request_id())
+            }
+        };
+        if let Some(value) = request_id {
+            span.set_attribute(semcov::trace::AWS_REQUEST_ID.string(value.to_owned()));
+        }
+        span.set_attribute(KeyValue::new("success", status == Status::Ok));
+        span.set_status(status);
+    }
 }

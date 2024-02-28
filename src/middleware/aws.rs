42ae49ee2275aceccd6dfed3a0c8d86f4c4a84bb
Leonid Beschasny <Leonid.Beschastny@viaplaygroup.com>
2024-02-28 11:11:38 +0100

    Improve aws span implementation (#12)
    * Use semantic conventions for aws spans attributes
    
    * Refactor AwsSpan to use builder pattern
    
    * Make AwsSpan more generic
    
    * Add readme
    
    * Follow semantic convention

--- src/middleware/aws.rs
+++ src/middleware/aws.rs
@@ -2,86 +2,136 @@ use aws_types::request_id::RequestId;
 use opentelemetry::{
     global::{self, BoxedSpan, BoxedTracer},
     trace::{Span as TelemetrySpan, SpanBuilder, SpanKind, Status, Tracer},
-    Context, KeyValue,
 };
-use opentelemetry_semantic_conventions as semcov;
 use std::error::Error;
 use tracing::Span;
-use tracing_opentelemetry::OpenTelemetrySpanExt;
 
-pub enum AwsTarget<'a> {
-    Dynamo(&'a str),
-    Firehose(&'a str),
-    Sns(&'a str),
+use crate::{semcov, Context, KeyValue, OpenTelemetrySpanExt, StringValue};
+
+pub enum AwsTarget<T: Into<StringValue>> {
+    Dynamo(T),
+    Firehose(T),
+    Sns(T),
 }
 
-impl AwsTarget<'_> {
-    pub fn system(&self) -> &'static str {
+pub trait IntoAttributes {
+    fn service(&self) -> &'static str;
+    fn span_kind(&self) -> SpanKind {
+        SpanKind::Client
+    }
+    fn into_attributes(self, method: &'static str) -> Vec<KeyValue>;
+}
+
+impl<T: Into<StringValue>> IntoAttributes for AwsTarget<T> {
+    fn service(&self) -> &'static str {
         match self {
-            AwsTarget::Dynamo(_) => "dynamodb",
-            AwsTarget::Firehose(_) => "firehose",
-            AwsTarget::Sns(_) => "sns",
+            AwsTarget::Dynamo(_) => "DynamoDB",
+            AwsTarget::Firehose(_) => "Firehose",
+            AwsTarget::Sns(_) => "SNS",
         }
     }
 
-    pub fn attributes(&self) -> Vec<KeyValue> {
+    fn span_kind(&self) -> SpanKind {
         match self {
-            AwsTarget::Dynamo(table_name) => vec![
-                KeyValue::new("dynamoDB", true),
-                KeyValue::new("db.name", table_name.to_string()),
-            ],
+            AwsTarget::Dynamo(_) => SpanKind::Client,
+            AwsTarget::Firehose(_) => SpanKind::Producer,
+            AwsTarget::Sns(_) => SpanKind::Producer,
+        }
+    }
+
+    fn into_attributes(self, method: &'static str) -> Vec<KeyValue> {
+        match self {
+            AwsTarget::Dynamo(table_name) => {
+                let table_name: StringValue = table_name.into();
+                vec![
+                    semcov::DB_SYSTEM.string("dynamodb"),
+                    semcov::DB_NAME.string(table_name.clone()),
+                    semcov::DB_OPERATION.string(method),
+                    semcov::AWS_DYNAMODB_TABLE_NAMES.array(vec![table_name]),
+                ]
+            }
             AwsTarget::Firehose(stream_name) => vec![
-                KeyValue::new("firehose", true),
-                KeyValue::new("firehose.name", stream_name.to_string()),
+                semcov::MESSAGING_SYSTEM.string("aws_firehose"),
+                semcov::MESSAGING_OPERATION.string("publish"),
+                semcov::MESSAGING_DESTINATION_NAME.string(stream_name),
             ],
             AwsTarget::Sns(topic_arn) => vec![
-                KeyValue::new("sns", true),
-                KeyValue::new("sns.topic.arn", topic_arn.to_string()),
+                semcov::MESSAGING_SYSTEM.string("aws_sns"),
+                semcov::MESSAGING_OPERATION.string("publish"),
+                semcov::MESSAGING_DESTINATION_NAME.string(topic_arn),
             ],
         }
     }
 }
 
-pub enum AwsSpan {
-    Pending(Box<SpanBuilder>, BoxedTracer),
-    Started(BoxedSpan),
+pub struct AwsSpanBuilder {
+    inner: SpanBuilder,
+    tracer: BoxedTracer,
+}
+
+impl AwsSpanBuilder {
+    pub fn new(aws_target: impl IntoAttributes, method: impl Into<&'static str>) -> Self {
+        let tracer = global::tracer("aws_sdk");
+        let service = aws_target.service();
+        let method: &'static str = method.into();
+        let span_name = format!("{service}.{method}");
+        let span_kind = aws_target.span_kind();
+        let mut attributes = aws_target.into_attributes(method);
+        attributes.extend(vec![
+            semcov::RPC_METHOD.string(method),
+            semcov::RPC_SYSTEM.string("aws-api"),
+            semcov::RPC_SERVICE.string(service),
+        ]);
+        let inner = tracer
+            .span_builder(span_name)
+            .with_attributes(attributes)
+            .with_kind(span_kind);
+
+        Self { inner, tracer }
+    }
+
+    pub fn set_attribute(&mut self, attribute: KeyValue) {
+        if let Some(attributes) = &mut self.inner.attributes {
+            attributes.push(attribute);
+        }
+    }
+
+    pub fn start_with_context(self, parent_cx: &Context) -> AwsSpan {
+        self.inner
+            .start_with_context(&self.tracer, parent_cx)
+            .into()
+    }
+
+    pub fn start(self) -> AwsSpan {
+        self.start_with_context(&Span::current().context())
+    }
+}
+
+pub struct AwsSpan {
+    span: BoxedSpan,
 }
 
 impl AwsSpan {
-    pub fn new(aws_target: AwsTarget, operation: &str, method: &str) -> Self {
-        let tracer = global::tracer("aws_sdk");
-        let system = aws_target.system();
-        let mut attributes = vec![
-            semcov::trace::RPC_METHOD.string(method.to_string()),
-            semcov::trace::RPC_SYSTEM.string("aws-api"),
-            semcov::trace::RPC_SERVICE.string(system),
-            KeyValue::new("aws_operation", operation.to_string()),
-            KeyValue::new("db.system", system),
-            KeyValue::new("db.operation", method.to_string()),
-        ];
-        attributes.extend(aws_target.attributes());
-        let span_builder = tracer
-            .span_builder(format!("aws_{system}"))
-            .with_attributes(attributes)
-            .with_kind(SpanKind::Client);
-
-        Self::Pending(Box::new(span_builder), tracer)
+    #[inline]
+    pub fn build(
+        aws_target: impl IntoAttributes,
+        method: impl Into<&'static str>,
+    ) -> AwsSpanBuilder {
+        AwsSpanBuilder::new(aws_target, method)
     }
 
-    pub fn start_with_context(self, parent_cx: &Context) -> Self {
-        let span = match self {
-            AwsSpan::Pending(builder, tracer) => {
-                builder.start_with_context(&tracer, parent_cx)
-            }
-            AwsSpan::Started(_) => {
-                panic!("Span already started");
-            }
-        };
-        AwsSpan::Started(span)
+    #[inline]
+    pub fn new(aws_target: impl IntoAttributes, method: impl Into<&'static str>) -> Self {
+        Self::build(aws_target, method).start()
     }
 
-    pub fn start(self) -> Self {
-        self.start_with_context(&Span::current().context())
+    #[inline]
+    pub fn with_context(
+        aws_target: impl IntoAttributes,
+        method: impl Into<&'static str>,
+        parent_cx: &Context,
+    ) -> Self {
+        Self::build(aws_target, method).start_with_context(parent_cx)
     }
 
     pub fn end<T, E>(self, aws_response: &Result<T, E>)
@@ -89,12 +139,7 @@ impl AwsSpan {
         T: RequestId,
         E: RequestId + Error,
     {
-        let mut span = match self {
-            AwsSpan::Pending(_, _) => {
-                panic!("Span not started");
-            }
-            AwsSpan::Started(span) => span,
-        };
+        let mut span = self.span;
         let (status, request_id) = match aws_response {
             Ok(resp) => (Status::Ok, resp.request_id()),
             Err(error) => {
@@ -103,9 +148,15 @@ impl AwsSpan {
             }
         };
         if let Some(value) = request_id {
-            span.set_attribute(semcov::trace::AWS_REQUEST_ID.string(value.to_owned()));
+            span.set_attribute(semcov::AWS_REQUEST_ID.string(value.to_owned()));
         }
-        span.set_attribute(KeyValue::new("success", status == Status::Ok));
         span.set_status(status);
+    }
+}
+
+impl From<BoxedSpan> for AwsSpan {
+    #[inline]
+    fn from(span: BoxedSpan) -> Self {
+        Self { span }
     }
 }

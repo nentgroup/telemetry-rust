b01fdeed712c7602aae84e4acd5d46a006393f0a
Leonid Beschastny <Leonid.Beschastny@viaplaygroup.com>
2024-04-15 19:46:48 +0200

    Flush otel data after each lambda invocation

--- src/middleware/lambda.rs
+++ src/middleware/lambda.rs
@@ -1,14 +1,20 @@
 use crate::semconv;
 use lambda_runtime::LambdaInvocation;
-use std::task::{Context as TaskContext, Poll};
+use opentelemetry_sdk::trace::TracerProvider;
+use pin_project_lite::pin_project;
+use std::future::Future;
+use std::pin::Pin;
+use std::task::{ready, Context as TaskContext, Poll};
 use tower::{Layer, Service};
 use tracing::{instrument::Instrumented, Instrument};
 
-pub struct OtelLambdaLayer {}
+pub struct OtelLambdaLayer {
+    provider: TracerProvider,
+}
 
 impl OtelLambdaLayer {
-    pub fn new() -> Self {
-        Self {}
+    pub fn new(provider: TracerProvider) -> Self {
+        Self { provider }
     }
 }
 
@@ -18,6 +24,7 @@ impl<S> Layer<S> for OtelLambdaLayer {
     fn layer(&self, inner: S) -> Self::Service {
         OtelLambdaService {
             inner,
+            provider: self.provider.clone(),
             coldstart: true,
         }
     }
@@ -25,6 +32,7 @@ impl<S> Layer<S> for OtelLambdaLayer {
 
 pub struct OtelLambdaService<S> {
     inner: S,
+    provider: TracerProvider,
     coldstart: bool,
 }
 
@@ -34,7 +42,7 @@ where
 {
     type Response = ();
     type Error = S::Error;
-    type Future = Instrumented<S::Future>;
+    type Future = OtelLambdaFuture<Instrumented<S::Future>>;
 
     fn poll_ready(&mut self, cx: &mut TaskContext<'_>) -> Poll<Result<(), Self::Error>> {
         self.inner.poll_ready(cx)
@@ -51,6 +59,40 @@ where
 
         self.coldstart = false;
 
-        self.inner.call(req).instrument(span)
+        let future = self.inner.call(req).instrument(span);
+        OtelLambdaFuture {
+            future: Some(future),
+            provider: self.provider.clone(),
+        }
+    }
+}
+
+pin_project! {
+    pub struct OtelLambdaFuture<F> {
+        #[pin]
+        future: Option<F>,
+        provider: TracerProvider,
+    }
+}
+
+impl<F: Future> Future for OtelLambdaFuture<F> {
+    type Output = F::Output;
+
+    fn poll(mut self: Pin<&mut Self>, cx: &mut TaskContext<'_>) -> Poll<Self::Output> {
+        // First, try to get the ready value of the future
+        let ready = ready!(self
+            .as_mut()
+            .project()
+            .future
+            .as_pin_mut()
+            .expect("future polled after completion")
+            .poll(cx));
+
+        // If we got the ready value, we first drop the future: this ensures that the
+        // OpenTelemetry span attached to it is closed and included in the subsequent flush.
+        Pin::set(&mut self.as_mut().project().future, None);
+
+        self.provider.force_flush();
+        Poll::Ready(ready)
     }
 }

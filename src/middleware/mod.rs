7e9d75efb555487f1db2fe6c45f2ad93a240cb9d
Leonid Beschastny <Leonid.Beschastny@viaplaygroup.com>
2024-05-15 11:07:03 +0200

    Run cargo fmt

--- src/middleware/mod.rs
+++ src/middleware/mod.rs
@@ -1,6 +1,8 @@
 #[cfg(feature = "aws-span")]
 pub mod aws;
-#[cfg(feature = "aws-lambda")]
-pub mod lambda;
+
 #[cfg(feature = "axum")]
 pub mod axum;
+
+#[cfg(feature = "aws-lambda")]
+pub mod lambda;

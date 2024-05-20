b2c60930e35ca5faf5dc981d8176f5f34f8b47b4
Leonid Beschastny <Leonid.Beschastny@viaplaygroup.com>
2024-05-20 16:37:37 +0200

    Make OtelLambdaLayer more generic

--- src/middleware/lambda/mod.rs
+++ src/middleware/lambda/mod.rs
@@ -5,27 +5,57 @@ use crate::{
 use lambda_runtime::LambdaInvocation;
 use opentelemetry::trace::SpanKind;
 use opentelemetry_sdk::trace::TracerProvider;
-use std::task::{Context as TaskContext, Poll};
+use std::{
+    sync::Arc,
+    task::{Context as TaskContext, Poll},
+};
 use tower::{Layer, Service};
-use tracing::{instrument::Instrumented, Instrument};
+use tracing::{instrument::Instrumented, Instrument, Span};
 use tracing_opentelemetry_instrumentation_sdk::TRACING_TARGET;
 
-pub struct OtelLambdaLayer {
-    provider: TracerProvider,
+trait OtelLambdaServiceContext {
+    fn create_span(&self, req: &LambdaInvocation, coldstart: bool) -> Span;
 }
 
-impl OtelLambdaLayer {
-    pub fn new(provider: TracerProvider) -> Self {
-        Self { provider }
+pub struct GenericLambdaInvocation {}
+
+impl OtelLambdaServiceContext for GenericLambdaInvocation {
+    #[inline]
+    fn create_span(&self, req: &LambdaInvocation, coldstart: bool) -> Span {
+        tracing::trace_span!(
+            target: TRACING_TARGET,
+            "Lambda function invocation",
+            // TODO: set correct otel.kind and faas.trigger
+            // see https://opentelemetry.io/docs/specs/semconv/faas/aws-lambda/
+            "otel.kind" = ?SpanKind::Server,
+            "otel.name" = req.context.env_config.function_name,
+            { semconv::FAAS_TRIGGER } = "other",
+            { semconv::AWS_LAMBDA_INVOKED_ARN } = req.context.invoked_function_arn,
+            { semconv::FAAS_INVOCATION_ID } = req.context.request_id,
+            { semconv::FAAS_COLDSTART } = coldstart,
+        )
     }
 }
 
-impl<S> Layer<S> for OtelLambdaLayer {
-    type Service = OtelLambdaService<S>;
+pub struct OtelLambdaLayer<C> {
+    context: Arc<C>,
+    provider: TracerProvider,
+}
+
+impl OtelLambdaLayer<GenericLambdaInvocation> {
+    pub fn new(provider: TracerProvider) -> Self {
+        let context = Arc::new(GenericLambdaInvocation {});
+        Self { context, provider }
+    }
+}
+
+impl<S, C> Layer<S> for OtelLambdaLayer<C> {
+    type Service = OtelLambdaService<S, C>;
 
     fn layer(&self, inner: S) -> Self::Service {
         OtelLambdaService {
             inner,
+            context: self.context.clone(),
             provider: self.provider.clone(),
             coldstart: true,
         }
@@ -38,15 +68,17 @@ impl<T> InstrumentedFutureContext<T> for TracerProvider {
     }
 }
 
-pub struct OtelLambdaService<S> {
+pub struct OtelLambdaService<S, C> {
     inner: S,
+    context: Arc<C>,
     provider: TracerProvider,
     coldstart: bool,
 }
 
-impl<S, R> Service<LambdaInvocation> for OtelLambdaService<S>
+impl<S, R, C> Service<LambdaInvocation> for OtelLambdaService<S, C>
 where
     S: Service<LambdaInvocation, Response = R>,
+    C: OtelLambdaServiceContext,
 {
     type Response = R;
     type Error = S::Error;
@@ -57,18 +89,7 @@ where
     }
 
     fn call(&mut self, req: LambdaInvocation) -> Self::Future {
-        let span = tracing::trace_span!(
-            target: TRACING_TARGET,
-            "Lambda function invocation",
-            // TODO: set correct otel.kind and faas.trigger
-            // see https://opentelemetry.io/docs/specs/semconv/faas/aws-lambda/
-            "otel.kind" = ?SpanKind::Server,
-            "otel.name" = req.context.env_config.function_name,
-            { semconv::FAAS_TRIGGER } = "other",
-            { semconv::AWS_LAMBDA_INVOKED_ARN } = req.context.invoked_function_arn,
-            { semconv::FAAS_INVOCATION_ID } = req.context.request_id,
-            { semconv::FAAS_COLDSTART } = self.coldstart,
-        );
+        let span = self.context.create_span(&req, self.coldstart);
 
         self.coldstart = false;
 

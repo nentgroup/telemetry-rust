7c6498e14fb75fdea6a3a95083a3ef4fbbc3f663
Leonid Beschastny <Leonid.Beschastny@viaplaygroup.com>
2024-10-04 15:13:41 +0200

    Reorder consts

--- src/semconv.rs
+++ src/semconv.rs
@@ -2,7 +2,7 @@ pub use opentelemetry_semantic_conventions::attribute::*;
 
 #[allow(deprecated)]
 pub mod legacy {
-    pub const MESSAGING_OPERATION: &str = super::MESSAGING_OPERATION;
     pub const DB_NAME: &str = super::DB_NAME;
     pub const DB_OPERATION: &str = super::DB_OPERATION;
+    pub const MESSAGING_OPERATION: &str = super::MESSAGING_OPERATION;
 }

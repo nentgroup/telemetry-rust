ba4ad5f3e40a1b6ff4fe5da677cc0eed5dcdbd32
Leonid Beschastny <Leonid.Beschastny@viaplaygroup.com>
2025-01-02 11:54:47 +0100

    Fix clippy warnings

--- src/middleware/aws/operations/sns.rs
+++ src/middleware/aws/operations/sns.rs
@@ -4,7 +4,7 @@ use super::*;
 
 pub enum SnsSpanBuilder {}
 
-impl<'a> AwsSpanBuilder<'a> {
+impl AwsSpanBuilder<'_> {
     pub fn sns(
         operation_kind: MessagingOperationKind,
         method: impl Into<StringValue>,

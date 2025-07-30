d81d89b73341182f8030a67edba70149cf337fc6
Leonid Beschastny <Leonid.Beschastny@viaplaygroup.com>
2025-07-31 01:08:48 +0200

    Run cargo fmt

--- src/middleware/aws/instrumentation/mod.rs
+++ src/middleware/aws/instrumentation/mod.rs
@@ -1,13 +1,13 @@
+#[cfg(feature = "aws-fluent-builder-instrumentation")]
+mod fluent_builder;
 #[cfg(feature = "aws-instrumentation")]
 mod future;
 #[cfg(feature = "aws-stream-instrumentation")]
 mod stream;
-#[cfg(feature = "aws-fluent-builder-instrumentation")]
-mod fluent_builder;
 
+#[cfg(feature = "aws-fluent-builder-instrumentation")]
+pub use fluent_builder::*;
 #[cfg(feature = "aws-instrumentation")]
 pub use future::*;
 #[cfg(feature = "aws-stream-instrumentation")]
 pub use stream::*;
-#[cfg(feature = "aws-fluent-builder-instrumentation")]
-pub use fluent_builder::*;

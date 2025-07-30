cf52f798eccc196b631568a52520f42b62f38858
Leonid Beschastny <Leonid.Beschastny@viaplaygroup.com>
2025-07-30 13:47:08 +0200

    Add missing docs

--- src/middleware/aws/instrumentation/fluent_builder/mod.rs
+++ src/middleware/aws/instrumentation/fluent_builder/mod.rs
@@ -9,32 +9,63 @@ mod firehose;
 #[cfg(feature = "aws-sns")]
 mod sns;
 
+/// A trait for AWS service clients that can be instrumented with OpenTelemetry tracing.
+///
+/// This trait provides methods to build spans for AWS operations and instrument the
+/// fluent builders returned by AWS SDK operations.
 pub trait AwsInstrumentBuilder<'a>
 where
     Self: Sized,
 {
+    /// Builds an AWS span for the specific operation represented by this builder.
+    ///
+    /// Returns an [`AwsSpanBuilder`] that contains the necessary span attributes
+    /// and metadata for the AWS operation.
     fn build_aws_span(&self) -> AwsSpanBuilder<'a>;
+
+    /// Instruments this fluent builder with OpenTelemetry tracing.
+    ///
+    /// Returns an [`InstrumentedFluentBuilder`] that will automatically create
+    /// and manage spans when the operation is executed.
     fn instrument(self) -> InstrumentedFluentBuilder<'a, Self> {
         let span = self.build_aws_span();
         InstrumentedFluentBuilder::new(self, span)
     }
 }
 
+/// A wrapper that instruments AWS fluent builders with OpenTelemetry tracing.
+///
+/// This struct wraps AWS SDK fluent builders and automatically creates spans
+/// when operations are executed, providing distributed tracing capabilities
+/// for AWS service calls.
 pub struct InstrumentedFluentBuilder<'a, T: AwsInstrumentBuilder<'a>> {
     inner: T,
     span: AwsSpanBuilder<'a>,
 }
 
 impl<'a, T: AwsInstrumentBuilder<'a>> InstrumentedFluentBuilder<'a, T> {
+    /// Creates a new instrumented fluent builder.
+    ///
+    /// # Arguments
+    /// * `inner` - The AWS SDK fluent builder to wrap
+    /// * `span` - The span builder with AWS operation metadata
     pub fn new(inner: T, span: AwsSpanBuilder<'a>) -> Self {
         Self { inner, span }
     }
 
+    /// Sets the OpenTelemetry context for this instrumented builder.
+    ///
+    /// # Arguments
+    /// * `context` - The OpenTelemetry context to use for span creation
     pub fn context(mut self, context: &'a Context) -> Self {
         self.span = self.span.context(context);
         self
     }
 
+    /// Sets the OpenTelemetry context for this instrumented builder.
+    ///
+    /// # Arguments
+    /// * `context` - Optional OpenTelemetry context to use for span creation
     pub fn set_context(mut self, context: Option<&'a Context>) -> Self {
         self.span = self.span.set_context(context);
         self

2ce266c21a9c2d88ca9a28d2f0feaef02bb8319d
Leonid Beschastny <Leonid.Beschastny@viaplaygroup.com>
2026-07-16 17:38:07 +0200

    Remove unnecessary cfg

--- src/instrumentations/http/mod.rs
+++ src/instrumentations/http/mod.rs
@@ -1,11 +1,5 @@
 //! HTTP client instrumentation utilities.
 
-#[cfg(any(
-    feature = "reqwest",
-    feature = "hyper-http1",
-    feature = "hyper-http2",
-    feature = "hyper-client-legacy"
-))]
 mod client;
 
 #[cfg(feature = "reqwest")]

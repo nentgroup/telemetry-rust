6f010520f56e40e9c3555db5394367fa62f98419
Travispersson <travis.persson@viaplaygroup.com>
2026-07-27 13:04:18 +0200

    enable legacy attrs for now

--- src/middleware/aws/mod.rs
+++ src/middleware/aws/mod.rs
@@ -228,6 +228,10 @@ impl<'a> AwsSpanBuilder<'a> {
         let mut attributes = vec![
             KeyValue::new(semconv::RPC_METHOD, method),
             KeyValue::new(semconv::RPC_SYSTEM_NAME, "aws-api"),
+            #[allow(deprecated)] // legacy attribute
+            KeyValue::new(semconv::RPC_SYSTEM, "aws-api"),
+            #[allow(deprecated)] // legacy attribute
+            KeyValue::new(semconv::RPC_SERVICE, service),
         ];
         attributes.extend(custom_attributes);
         let inner = tracer
